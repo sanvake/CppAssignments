a705796f09a10d24d0d1f6d253a7a5ed7f98ee70
Vake <64536251+sanvake@users.noreply.github.com>
2020-11-08 23:32:14 +0530

    Update textcompiler.py
    added comments

--- textcompiler.py
+++ textcompiler.py
@@ -1,11 +1,12 @@
 import os
+# change assignment/ to name of folder where cpp files are saved
 p="assignment/"
 pathlist = sorted(os.listdir(p), key = lambda x: int("".join([i for i in x if i.isdigit()])))
 j=1
 name="Vakesan M"
 regno="18GAEM9089"
 intro=name+"\n"+regno
-
+# change concatfile.txt to whatever output file name you desire
 with open('concatfile.txt','a') as outfile:
     outfile.write(intro)
     for path in pathlist:
